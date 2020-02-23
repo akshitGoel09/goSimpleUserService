a543478831c4e6f5c97bd4481de7d7f28bcd78d7
Akshit <akshit@Akshits-MBP.Dlink>
2020-02-23 10:31:36 +0530

    simple server

--- main.go
+++ main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 
-	"github.com/akshitGoel09/goSimpleUserService/model"
+	"github.com/akshitGoel09/goSimpleUserService/controllers"
 )
 
 func main() {
-	u := model.User{
-		ID:        2,
-		FirstName: "Tricia",
-		LastName:  "McMillan",
-	}
-	fmt.Println(u)
+	controllers.RegisterControllers()
+	http.ListenAndServe(":3000", nil)
 }
